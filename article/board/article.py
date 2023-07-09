4310db6efb84f62e8f7868fb977272916a62e0e9
dhalsdl12 <dhalsdl12@gmail.com>
2023-07-09 23:10:15 +0900

    크롤링 성능 향상

--- article/board/article.py
+++ article/board/article.py
@@ -1,24 +1,34 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def pageCrawl(drive):
+def pageCrawl(driver):
     article_title = []
     article_link = []
     article_text = []
-    article_list = drive.find_elements(By.XPATH, "//ul[@class=\"list_news\"]/li")
-    for article in article_list:
-        try:
-            news_info = article.find_element(By.XPATH, "./div/div/a[@class=\"news_tit\"]")
-            news_text = article.find_element(By.XPATH, "./div/div/div[@class=\"news_dsc\"]/div/a")
-            title = news_info.get_attribute('title')
-            link = news_info.get_attribute('href')
-            article_title.append(title)
-            article_link.append(link)
-            article_text.append(news_text.text[:80])
-        except:
-            print("error")
+
+    try:
+        article_list = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.list_news li"))
+        )
+        for article in article_list:
+            try:
+                news_info = article.find_element(By.CSS_SELECTOR, "div div a.news_tit")
+                news_text = article.find_element(By.CSS_SELECTOR, "div div div.news_dsc div a")
+                title = news_info.get_attribute('title')
+                link = news_info.get_attribute('href')
+                article_title.append(title)
+                article_link.append(link)
+                article_text.append(news_text.text)
+            except:
+                print("Error while parsing article")
+
+    except:
+        print("Error while fetching article list")
+
     return article_title, article_link, article_text
 
 
@@ -31,20 +41,28 @@ def start(title):
 
     chrome_driver = os.path.join('chromedriver')
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')  # 브라우저 창을 표시하지 않도록 설정 (headless 모드)
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
-    drive = webdriver.Chrome(chrome_driver, options=chrome_options)
-    drive.get(url)
+    driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+    driver.get(url)
 
-    ele = drive.find_element(By.XPATH, '//*[@id="nx_query"]')
+    ele = driver.find_element(By.CSS_SELECTOR, '#nx_query')
     ele.send_keys(title)
     ele.submit()
-    drive.find_element(By.XPATH, '//*[@id="snb"]/div[1]/div/div[1]/a[2]').click()
 
-    article_title, article_link, article_text = pageCrawl(drive)
+    try:
+        news_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#snb > div:nth-child(1) > div > div:nth-child(1) > a:nth-child(2)'))
+        )
+        news_button.click()
 
-    drive.close()
+        article_title, article_link, article_text = pageCrawl(driver)
+
+    except:
+        print("Error while loading news page")
+
+    driver.close()
 
     return article_title, article_link, article_text
